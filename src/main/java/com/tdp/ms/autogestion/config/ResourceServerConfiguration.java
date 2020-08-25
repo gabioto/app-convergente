55c68b25a358cb2c86c398c927b5bc9be6bf83b5
Augusto Alva Campos <afalva@indracompany.com>
2020-08-25 13:22:34 -0500

    Cambios seguridad

---
+++ src/main/java/com/tdp/ms/autogestion/config/ResourceServerConfiguration.java
@@ -0,0 +1,50 @@
+package com.tdp.ms.autogestion.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class ResourceServerConfiguration extends GlobalMethodSecurityConfiguration {
+
+	@Configuration
+	public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+
+			http.authorizeRequests().antMatchers(HttpMethod.GET, " /**")
+					.permitAll().antMatchers(HttpMethod.POST, "/**")
+					.permitAll().antMatchers(HttpMethod.PUT, "/**")
+					.permitAll().antMatchers(HttpMethod.PATCH, "/**")
+					.permitAll().antMatchers(HttpMethod.DELETE, "/**")
+					.permitAll();
+		}
+
+		@Bean
+		CorsConfigurationSource corsConfigurationSource() {
+			CorsConfiguration configuration = new CorsConfiguration();
+			configuration.setAllowedOrigins(Arrays.asList("*"));
+			configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "HEAD", "OPTIONS"));
+			configuration.setAllowedHeaders(Arrays.asList("*"));
+			configuration.setExposedHeaders(Arrays.asList("id_captcha", "download_file_name"));
+
+			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			source.registerCorsConfiguration("/**", configuration);
+			return source;
+		}
+	}
+
+}
