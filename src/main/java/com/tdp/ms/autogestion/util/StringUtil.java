e2015187e711e0acc05569c1a5d0892d6797279f
Augusto Alva Campos <afalva@indracompany.com>
2020-09-01 16:06:40 -0500

    Primeras pruebas unitarias

--- src/main/java/com/tdp/ms/autogestion/util/StringUtil.java
+++ src/main/java/com/tdp/ms/autogestion/util/StringUtil.java
@@ -1,5 +1,9 @@
 package com.tdp.ms.autogestion.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class StringUtil {
 
 	public static String validateEmptyField(String field) {
