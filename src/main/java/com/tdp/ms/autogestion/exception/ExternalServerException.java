4c3ba626a4a60383cadb443676a93fccbc927488
Augusto Alva Campos <afalva@indracompany.com>
2020-09-11 17:51:59 -0500

    Configuracion propiedades prod

--- src/main/java/com/tdp/ms/autogestion/exception/ExternalServerException.java
+++ src/main/java/com/tdp/ms/autogestion/exception/ExternalServerException.java
@@ -2,6 +2,8 @@ package com.tdp.ms.autogestion.exception;
 
 public class ExternalServerException extends DomainException{
 
+	private static final long serialVersionUID = -4421133771413546676L;
+
 	public ExternalServerException(ErrorCategory error,
 			String userMessage) {
 		super(userMessage, error);
--- src/main/java/com/tdp/ms/autogestion/exception/ValidRequestException.java
+++ src/main/java/com/tdp/ms/autogestion/exception/ValidRequestException.java
@@ -2,6 +2,8 @@ package com.tdp.ms.autogestion.exception;
 
 public class ValidRequestException extends DomainException{
 
+	private static final long serialVersionUID = 5876962427698288005L;
+
 	public ValidRequestException(ErrorCategory error,
 			String userMessage) {
 		super(userMessage, error);
